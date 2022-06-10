19be3443171ca619f1c80e68290c180e3a735d9d
Lucas <lucasvalenteds@gmail.com>
2022-06-10 00:13:24 -0300

    Creating Application class and test

---
+++ src/test/java/com/example/ApplicationTest.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@SpringBootTest
+@Testcontainers
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class ApplicationTest {
+
+    @Container
+    private static final PostgreSQLContainer<?> CONTAINER =
+            new PostgreSQLContainer<>(DockerImageName.parse("postgres"));
+
+    @DynamicPropertySource
+    private static void setDatasourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", CONTAINER::getUsername);
+        registry.add("spring.datasource.password", CONTAINER::getPassword);
+    }
+
+    @Test
+    void contextLoads() {
+    }
+}
